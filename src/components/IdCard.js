d21e59089c72d7a9c5359085f2f9f5fd5a5aa0df
Daniel Cornejo <contact@danielcornejo.dev>
2023-07-01 15:45:10 -0400

    iteration 1 complete

---
+++ src/components/IdCard.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function IdCard({lastName, firstName, gender, height, birth, picture }) {
+  return (
+    <div className='card-cnt'>
+        <div className='inner-card'>
+            <img src={picture} />
+            <div className='inner-card-data'>
+                <h3>First Name: {firstName}</h3>
+                <h3>Last Name: {lastName}</h3>
+                <h3>Gender: {gender}</h3>
+                <h3>Height: {height}</h3>
+                <h3>Birth: {birth.toDateString()}</h3>
+            </div>
+        </div>
+    </div>
+  )
+}
